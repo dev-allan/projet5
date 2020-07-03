f5648c6afaed877bb058c9ff47ad9515e4f3d39d
Allan Duminy <allanduminy@protonmail.com>
2020-07-03 16:44:35 +0200

    Mise en place du js pour la page des produits et description des produits

---
+++ front-end/index.js
@@ -0,0 +1,39 @@
+const listCamera = document.getElementById('listCamera');
+const img = document.querySelector('img');
+
+let Resquest = new XMLHttpRequest();
+
+Resquest.open('GET', 'http://localhost:3000/api/cameras')
+Resquest.onload = function() {
+  let theData = JSON.parse(Resquest.responseText);
+  renderHTML(theData);
+};
+  
+Resquest.send();
+
+console.log(Resquest);
+
+function renderHTML(data) {
+  for (let i = 0; i < data.length; i++) {
+    listCamera.innerHTML += `<li class="articles__lists--item cards__item">
+    <div class="cards__item__thumb">
+    <img src = "${data[i].imageUrl}" alt = "image d'un appareil photo ancien" style="heigth:200px; width:200px;"/>
+    </div>
+    <div class="cards__item__body">
+      <h3 class="cards__item__body--title">Appareil photo vintage ${data[i].name}</h3>
+      <p class="cards__item__body--name"><strong>Marque : </strong>${data[i].name}</p>
+      <p class="cards__item__body--lenses"><strong>Lentilles : </strong>${data[i].lenses}</p>
+      <p class="cards__item__body--description"><strong>Description : </strong>${data[i].description.slice(31)}...</p>
+      <p class="cards__item__body--price"><strong>Prix : </strong>${data[i].price}€</p>
+      <div class="cards__item--button">
+      <a class="btn" href="produit.html?id=${data[i]._id}" aria-label="Sélectionner l’appareil photo vintage ${
+      data[i].name
+    }">Voir les détails</a>
+      </a>
+    </div>
+    </div>
+  </li>`;
+  }
+
+};
+
---
+++ front-end/produit.js
@@ -0,0 +1,51 @@
+const detailDuProduit = document.getElementById('descriptionCamera');
+const img = document.querySelector('img');
+
+//fonction permettant de récupérer l'ID de l'url
+function getParamByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+let id = getParamByName('id');
+
+let Resquest = new XMLHttpRequest();
+
+Resquest.open('GET', "http://localhost:3000/api/cameras/"+id, true)
+Resquest.onload = function() {
+  let theData = JSON.parse(Resquest.responseText);
+  renderHTML(theData);
+};
+  
+Resquest.send();
+
+function renderHTML(data) {
+      detailDuProduit.innerHTML += `<li class="articles__lists--item cards__item">
+      <div class="cards__item__thumb">
+      <img src = "${data.imageUrl}" alt = "image d'un appareil photo ancien" style="heigth:200px; width:200px;"/>
+      </div>
+      <div class="cards__item__body">
+        <h3 class="cards__item__body--title">Appareil photo vintage ${data.name}</h3>
+        <p class="cards__item__body--name"><strong>Marque : </strong>${data.name}</p>
+        <p class="cards__item__body--lenses"><strong>Lentilles : </strong>${data.lenses}</p>
+        <p class="cards__item__body--description"><strong>Description : </strong>${data.description.slice(31)}...</p>
+        <p class="cards__item__body--price"><strong>Prix : </strong>${data.price}€</p>
+        <div class="cards__item--button">
+        </a>
+      </div>
+      </div>
+    </li>`;
+    // test option
+    for (let i = 0; i < data.lenses.length; i++) {
+      detailDuProduit.innerHTML +=  `<input type="radio" name="lentille" value="${data.lenses[i]}" id="${data.lenses[i]}" /> <label for="${data.lenses[i]}">${data.lenses[i]}</label><br />`
+    }
+    detailDuProduit.innerHTML += `<button type="button"  data-id="${id}" data-name="${data.name}" data-price="${data.price}" data-weight="97">Ajouter au panier</button>
+    `
+};
+
+  console.log(detailDuProduit);
